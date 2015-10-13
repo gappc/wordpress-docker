215baf933caa114fdccea161fa28c66936908798
gappc <gapp.christian@gmail.com>
2015-10-13 15:02:00 +0200

    Adjusted wordpress DB_HOST configuration

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'example');
 
 /** MySQL hostname */
-define('DB_HOST', 'db:3306');
+define('DB_HOST', 'mysql');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
